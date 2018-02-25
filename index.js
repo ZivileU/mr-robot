ce7b476c0b3e573888a991391ee5fa397f05d897
Zivile Utkeviciute <zivile.utkeviciute@gmail.com>
2018-02-25 13:09:59 +0100

    episode list created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function createNode(el) {
 }
 
 function append(parent, el) {
-  return parent.appendChild(el);
+    return parent.appendChild(el);
 }
 
 const root = document.getElementById('application-root');
@@ -11,33 +11,50 @@ const url = 'http://api.tvmaze.com/singlesearch/shows?q=mr-robot&embed=episodes'
 
 fetch(url)
     .then((resp) => resp.json())
-    .then(function(data) {
-        getTop(data);
-})
-.catch(function(error) {
-    console.log(error);
+    // .then((data) => getTop(data))
+    .then((data) => getEpisodes(data))
+    .catch(function(error) {
+        console.log(error);
 }); 
 
-function getTop(data){
-        let imgTop = createNode('img'),
-        descriptionDiv = createNode('div'),
-        name = createNode('h1'),
-        genres = createNode('p'),
-        rating = createNode('p'),
-        description = createNode('p');
+// function getTop(data){
+//     let imgTop = createNode('img'),
+//         descriptionDiv = createNode('div'),
+//         name = createNode('h1'),
+//         genres = createNode('p'),
+//         rating = createNode('p'),
+//         description = createNode('p');
 
-    imgTop.src = data.image.medium;
-    name.innerHTML = data.name;
-    genres.innerHTML = `<b>Genres:</b> ${data.genres.join(', ')}`;
-    rating.innerHTML = `<b>Rating:</b> ${Math.round(data.rating.average)}/10`;
-    description.innerHTML = data.summary;
+//     imgTop.src = data.image.medium;
+//     name.innerHTML = data.name;
+//     genres.innerHTML = `<b>Genres:</b> ${data.genres.join(', ')}`;
+//     rating.innerHTML = `<b>Rating:</b> ${Math.round(data.rating.average)}/10`;
+//     description.innerHTML = data.summary;
 
-    append(root, imgTop);
-    append(root, descriptionDiv);
-    append(descriptionDiv, name);
-    append(descriptionDiv, genres);
-    append(descriptionDiv, rating);
-    append(descriptionDiv, description);
+//     append(root, imgTop);
+//     append(root, descriptionDiv);
+//     append(descriptionDiv, name);
+//     append(descriptionDiv, genres);
+//     append(descriptionDiv, rating);
+//     append(descriptionDiv, description);
 
-    console.log(description.innerHTML = data.summary)
+//     console.log(data)
+// };
+
+function getEpisodes(data) {
+    let episodeList = data._embedded.episodes;
+    console.log(episodeList);
+
+    return episodeList.map(function(episode) {
+        let episodeDiv = createNode('div'),
+            episodeImg = createNode('img'),
+            episodeCaption = createNode('p');
+        episodeImg.src = episode.image.medium;
+        seasonNum = ("0" + episode.season).slice(-2);
+        episodeNum = ("0" + episode.number).slice(-2);
+        episodeCaption.innerHTML = `<b>S${seasonNum}E${episodeNum}</b> ${episode.name}`;
+        append(root, episodeDiv);
+        append(episodeDiv, episodeImg);
+        append(episodeDiv, episodeCaption);
+    })
 };
