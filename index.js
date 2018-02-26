33025077815b36890cb3551d34e957170dd3d6a8
Zivile Utkeviciute <zivile.utkeviciute@gmail.com>
2018-02-26 14:05:57 +0100

    description and episode list combined

--- index.js
+++ index.js
@@ -11,35 +11,35 @@ const url = 'http://api.tvmaze.com/singlesearch/shows?q=mr-robot&embed=episodes'
 
 fetch(url)
     .then((resp) => resp.json())
-    // .then((data) => getTop(data))
-    .then((data) => getEpisodes(data))
+    .then((data) => {getTop(data); getEpisodes(data)})
+    // .then((data) => getEpisodes(data))
     .catch(function(error) {
         console.log(error);
 }); 
 
-// function getTop(data){
-//     let imgTop = createNode('img'),
-//         descriptionDiv = createNode('div'),
-//         name = createNode('h1'),
-//         genres = createNode('p'),
-//         rating = createNode('p'),
-//         description = createNode('p');
+function getTop(data){
+    let imgTop = createNode('img'),
+        descriptionDiv = createNode('div'),
+        name = createNode('h1'),
+        genres = createNode('p'),
+        rating = createNode('p'),
+        description = createNode('p');
 
-//     imgTop.src = data.image.medium;
-//     name.innerHTML = data.name;
-//     genres.innerHTML = `<b>Genres:</b> ${data.genres.join(', ')}`;
-//     rating.innerHTML = `<b>Rating:</b> ${Math.round(data.rating.average)}/10`;
-//     description.innerHTML = data.summary;
+    imgTop.src = data.image.medium;
+    name.innerHTML = data.name;
+    genres.innerHTML = `<b>Genres:</b> ${data.genres.join(', ')}`;
+    rating.innerHTML = `<b>Rating:</b> ${Math.round(data.rating.average)}/10`;
+    description.innerHTML = data.summary;
 
-//     append(root, imgTop);
-//     append(root, descriptionDiv);
-//     append(descriptionDiv, name);
-//     append(descriptionDiv, genres);
-//     append(descriptionDiv, rating);
-//     append(descriptionDiv, description);
+    append(root, imgTop);
+    append(root, descriptionDiv);
+    append(descriptionDiv, name);
+    append(descriptionDiv, genres);
+    append(descriptionDiv, rating);
+    append(descriptionDiv, description);
 
-//     console.log(data)
-// };
+    console.log(data)
+};
 
 function getEpisodes(data) {
     let episodeList = data._embedded.episodes;
@@ -48,10 +48,10 @@ function getEpisodes(data) {
     return episodeList.map(function(episode) {
         let episodeDiv = createNode('div'),
             episodeImg = createNode('img'),
-            episodeCaption = createNode('p');
+            episodeCaption = createNode('p'),
+            seasonNum = ("0" + episode.season).slice(-2),
+            episodeNum = ("0" + episode.number).slice(-2);
         episodeImg.src = episode.image.medium;
-        seasonNum = ("0" + episode.season).slice(-2);
-        episodeNum = ("0" + episode.number).slice(-2);
         episodeCaption.innerHTML = `<b>S${seasonNum}E${episodeNum}</b> ${episode.name}`;
         append(root, episodeDiv);
         append(episodeDiv, episodeImg);
